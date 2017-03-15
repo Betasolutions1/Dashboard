8d4db35400c2bf79b32e85fd215046421ba88935
Betasolutions1 <beta.solutions@mail.com>
2017-03-15 10:47:56 +0530

    index page modified

--- mms/index.php
+++ mms/index.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1, user-scalable=0"> 
         <meta name="keywords" content=""> 
         <meta name="description" content=""> 
-        <title>MMS - &amp;  My Multi Solutions</title>         
+        <title>MMS - My Multi Solutions</title>         
         <!-- FAVICON AND APPLE TOUCH -->         
         <link rel="shortcut icon" href="images/icons/favicon.png"> 
         <link rel="apple-touch-icon-precomposed" sizes="57x57" href="images/icons/apple-touch-57x57.png"> 
